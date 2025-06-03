c512c7910032f3f747ae6e39396e4f42d3e3736e
gifcha <rudolfsbriedis77@gmail.com>
2025-06-03 18:07:58 +0300

    enabled cross origin

--- task-manager-backend/src/main/java/gifcha/vass_bootcamp_BE/task_manager_backend/Task/TaskController.java
+++ task-manager-backend/src/main/java/gifcha/vass_bootcamp_BE/task_manager_backend/Task/TaskController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
