595fa7a7d972e3102aeec69520660ebb18ff6dd6
gifcha <rudolfsbriedis77@gmail.com>
2025-07-10 15:26:56 +0300

    added token refreshing after each request

--- task-manager-backend/src/main/java/gifcha/vass_bootcamp_BE/task_manager_backend/Security/JwtAuthFilter.java
+++ task-manager-backend/src/main/java/gifcha/vass_bootcamp_BE/task_manager_backend/Security/JwtAuthFilter.java
@@ -18,39 +18,43 @@ import java.io.IOException;
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-	private JwtUtil jwtUtil;
-	private UserDetailsService userDetailsService;
+  private JwtUtil jwtUtil;
+  private UserDetailsService userDetailsService;
 
-	public JwtAuthFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
-		this.jwtUtil = jwtUtil;
-		this.userDetailsService = userDetailsService;
-	}
+  public JwtAuthFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+    this.jwtUtil = jwtUtil;
+    this.userDetailsService = userDetailsService;
+  }
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request,
-		HttpServletResponse response,
-		FilterChain filterChain
-	)
+  @Override
+  protected void doFilterInternal(HttpServletRequest request,
+    HttpServletResponse response,
+    FilterChain filterChain
+  )
 
-		throws ServletException, IOException {
-			String authHeader = request.getHeader("Authorization");
-			if (authHeader != null && authHeader.startsWith("Bearer ")) {
-				String token = authHeader.substring(7);
-				String username = jwtUtil.extractUsername(token);
+    throws ServletException, IOException {
+      String authHeader = request.getHeader("Authorization");
+      if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        String token = authHeader.substring(7);
+        String username = jwtUtil.extractUsername(token);
 
-				if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-					UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+          UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-					if (jwtUtil.validateToken(token, userDetails)) {
-						UsernamePasswordAuthenticationToken authToken =
-							new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-						authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          token = jwtUtil.refreshToken(token);
+          if (jwtUtil.validateToken(token, userDetails)) {
+            UsernamePasswordAuthenticationToken authToken =
+              new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-						SecurityContextHolder.getContext().setAuthentication(authToken);
-					}
-				}
-			}
 
-			filterChain.doFilter(request, response);
-		}
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+            response.setHeader("Authorization", "Bearer " + token); // set refreshed token in response
+          }
+        }
+      }
+
+      filterChain.doFilter(request, response);
+    }
+
 }
